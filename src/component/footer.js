be7474bdecdb268d53061d3becaffd8e3dd26389
4ssc <asai.lefty@gmail.com>
2021-02-03 09:20:02 +0900

    create footer.js

---
+++ src/component/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p className="p-2 text-center text-white bg-gray-500">&#169; 2021 asai.</p>
+    </footer>
+  );
+};
+
+export default Footer;
