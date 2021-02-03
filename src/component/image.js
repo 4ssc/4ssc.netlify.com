39e5c46b078e1c873f2f8484d726a9f815625eaa
4ssc <asai.lefty@gmail.com>
2021-02-03 10:12:50 +0900

    update image.js

--- src/component/image.js
+++ src/component/image.js
@@ -4,34 +4,36 @@ import Img from 'gatsby-image';
 
 const Image = (props) => {
   return(
-    <StaticQuery
-      query={graphql`
-        query {
-          images: allFile {
-            edges {
-              node {
-                relativePath
-                name
-                childImageSharp {
-                  fluid(maxWidth: 800) {
-                    ...GatsbyImageSharpFluid
+    <div className="image-wrapper">
+      <StaticQuery
+        query={graphql`
+          query {
+            images: allFile {
+              edges {
+                node {
+                  relativePath
+                  name
+                  childImageSharp {
+                    fluid(maxWidth: 800) {
+                      ...GatsbyImageSharpFluid
+                    }
                   }
                 }
               }
             }
-          }
-        }`
-      }
-      render={(data) => {
-        const image = data.images.edges.find(n => {
-          return n.node.relativePath.includes(props.filename);
-        });
-        if(!image) {return null;};
-        return(
-          <Img fluid={image.node.childImageSharp.fluid} alt={props.alt}/>
-        );
-      }}
-    />
+          }`
+        }
+        render={(data) => {
+          const image = data.images.edges.find(n => {
+            return n.node.relativePath.includes(props.filename);
+          });
+          if(!image) {return null;};
+          return(
+            <Img fluid={image.node.childImageSharp.fluid} alt={props.alt}/>
+          );
+        }}
+      />
+    </div>
   );
 };
 
