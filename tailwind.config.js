f955f6af7fc7c383b2af923de2a83701a377db8b
4ssc <asai.lefty@gmail.com>
2021-02-03 09:21:40 +0900

    create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    screens: {
+      '2xl': {'max': '1535px'},
+      'xl': {'max': '1279px'},
+      'lg': {'max': '1023px'},
+      'md': {'max': '767px'},
+      'sm': {'max': '639px'},
+    },
+    minHeight: {
+      '0': '0',
+      '1': '1rem',
+      '2': '2rem',
+      '3': '3rem',
+      '4': '4rem',
+      '5': '5rem',
+      '6': '6rem',
+      '7': '7rem',
+      '8': '8rem',
+      '9': '9rem',
+      'full': '100%',
+      'screen': '100vh',
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
