9b2e6a8c74957ff9210cf760bef664fed39f2d1b
4ssc <asai.lefty@gmail.com>
2021-02-03 09:20:37 +0900

    create image.js

---
+++ src/component/image.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
+
+const Image = (props) => {
+  return(
+    <StaticQuery
+      query={graphql`
+        query {
+          images: allFile {
+            edges {
+              node {
+                relativePath
+                name
+                childImageSharp {
+                  fluid(maxWidth: 800) {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+          }
+        }`
+      }
+      render={(data) => {
+        const image = data.images.edges.find(n => {
+          return n.node.relativePath.includes(props.filename);
+        });
+        if(!image) {return null;};
+        return(
+          <Img fluid={image.node.childImageSharp.fluid} alt={props.alt}/>
+        );
+      }}
+    />
+  );
+};
+
+export default Image;
